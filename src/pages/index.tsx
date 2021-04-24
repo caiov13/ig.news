6f622f26bcdc690a500c13764b34b2920c43ed55
Caio Vinicius <aio_vinicius11111@hotmail.com>
2021-04-24 00:59:05 -0300

    Change API call to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -21,7 +21,7 @@ export default function Home({ product }: HomeProps) {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
             <span>👏 Hey, welcome</span>
-            <h1>News about the <span>React</span> World</h1>
+            <h1>News about the <span>React</span> World.</h1>
             <p>Get access to all the publications <br/> <span>for {product.amount} month</span></p>
 
             <SubscribeButton priceId={product.priceId}/>
@@ -33,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IjXQTI7Grwwk7S1XwUMCRTC')
 
   const product = {
@@ -47,6 +47,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // Gera um HTML novo a cada 24 horas
   }
 }
